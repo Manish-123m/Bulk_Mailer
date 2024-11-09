05f5d573dba4558bd41b0f4e4e1c1975de7bb6af
Manish Kumar <147492862+Manish-123m@users.noreply.github.com>
2024-11-09 20:56:00 +0530

    Update settings.py

--- myemailproject/settings.py
+++ myemailproject/settings.py
@@ -128,7 +128,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.videcreators.com'  # e.g., 'smtp.gmail.com'
 EMAIL_PORT = 587  # or 465 for SSL
 EMAIL_USE_TLS = True  # or False if using SSL
-EMAIL_HOST_USER = 'manish@videcreators.com'
-EMAIL_HOST_PASSWORD = 'Mannu_dalal12'
-DEFAULT_FROM_EMAIL = 'manish@videcreators.com'
+EMAIL_HOST_USER = 'manishkumarmaurya9616@gmail.com'
+EMAIL_HOST_PASSWORD = 'Mann*****'
+DEFAULT_FROM_EMAIL = 'manishkumarmaurya9616@gmail.com'
 
